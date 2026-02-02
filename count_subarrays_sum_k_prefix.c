d18654ed423af929de8d994073c4f81880d6ea55
Ankit Sharma <sharmaji20202006@gmail.com>
2026-02-02 19:18:42 +0530

    Count subarrays with sum equal to K using prefix array

---
+++ count_subarrays_sum_k_prefix.c
@@ -0,0 +1,47 @@
+//Q. Find the number of subarrays with sum exactly equal to K (OPTIMIZED)
+
+#include <stdio.h>
+
+int main() {
+    int arr[100], prefix[100];
+    int n, k;
+
+    printf("Enter the length of the array: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the value of K: ");
+    scanf("%d", &k);
+
+    // Build prefix sum
+    prefix[0] = arr[0];
+    for(int i = 1; i < n; i++){
+        prefix[i] = prefix[i - 1] + arr[i];
+    }
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            int sum;
+
+            if(i == 0){
+                sum = prefix[j];
+            } else {
+                sum = prefix[j] - prefix[i - 1];
+            }
+
+            if(sum == k){
+                count++;
+            }
+        }
+    }
+
+    printf("count = %d\n", count);
+
+    return 0;
+}
